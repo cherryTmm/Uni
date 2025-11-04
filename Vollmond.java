a21a009b17a8dc93304a1d3d1f12f8dc27ee7044
Tim <timduenw@gmail.com>
2025-11-04 22:56:56 +0100

    Add Vollmond class with full moon calculation
    Implement main method to calculate full moons.

---
+++ Vollmond.java
@@ -0,0 +1,21 @@
+public class Vollmond{
+
+    public static void main(String[] args){
+        int n = Integer.parse.Int(args[0]);
+        int tag = 1;
+        int monat = 1;
+        int jahr = 2025;
+
+        int vollmond_gefunden = 0;
+
+        while(vollmond_gefunden < n){
+            tag += 29;
+            // tage müssen addiert werden 
+            // monat muss eins nach oben gehen 
+            // wenn monat voll dann auch jahr nach oben
+            // wenn monat mit 01..... beginnt dann 2 vollmonde oder monat hat 31 tage dann auch mit 02. 
+            
+        }
+
+    }
+}
